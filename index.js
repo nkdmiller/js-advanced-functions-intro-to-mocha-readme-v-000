edf070183c9c87faeb74c0df6fc5cf774e7d3f4a
Nate Miller <natekmiller96@gmail.com>
2018-10-17 19:46:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+function shouting(name) {
+  let result = name.toUpperCase();
+  return result;
+}
+function roundDown(num) {
+  let result = Math.floor(num);
+  return result;
+}
