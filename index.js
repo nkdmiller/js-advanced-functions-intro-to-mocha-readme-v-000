78fe34c1998397dd45ec5b540e6b04e7944fa121
Nate Miller <natekmiller96@gmail.com>
2018-10-17 19:40:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
